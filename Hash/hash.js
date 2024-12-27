639b3fb7c01e4b4b3b66aba33e1ca7fef4e775cd
“roy-sukrit” <roysukrit78@gmail.com>
2024-12-27 09:19:21 +0000

    Added HT

---
+++ Hash/hash.js
@@ -0,0 +1,90 @@
+class HashTable {
+    constructor(size = 7) {
+        this.dataMap = new Array(size);
+    }
+   
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+        }
+        return hash;
+    }
+
+    printTable() {
+        for (let i = 0; i < this.dataMap.length; i++) {
+            console.log(i, ": ", this.dataMap[i]);
+        }
+    }
+
+    set(key,value) {
+        let index = this._hash(key);
+        if(!this.dataMap[index]) this.dataMap[index] = [];
+        
+        this.dataMap[index].push([key, value]);
+        return this;
+    }
+
+	///  WRITE GET METHOD HERE  ///
+	//                           //
+	//                           //
+	//                           //
+	//                           //
+	///////////////////////////////
+	
+	get(key){
+	    let index = this._hash(key);
+	    
+	    if(this.dataMap[index]){
+	        for(let i =0;i<this.dataMap[index].length;i++){
+	            if(this.dataMap[index][i][0] === key){
+	                
+	                return this.dataMap[index][i][1]
+	            }
+	            
+	        }
+	    }
+	    return undefined;
+	}
+    keys(){
+
+        let allKeys=[]
+        for(let i=0;i<this.dataMap.length;i++){
+
+            if(this.dataMap[i]){
+            for(let j=0;j<this.dataMap[i].length;j++){
+
+                allKeys.push(this.dataMap[i][j][0])
+            }
+            }
+        }
+        return allKeys;
+    }
+
+}
+
+
+
+let myHashTable = new HashTable();
+
+myHashTable.set("nails", 100);
+myHashTable.set("tile", 50);
+myHashTable.set("lumber", 80);
+
+console.log("Lumber:");
+console.log( myHashTable.get("lumber") );
+
+console.log("\nBolts:");
+console.log( myHashTable.get("bolts") );
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Lumber:
+    80
+
+    Bolts:
+    undefined
+
+*/
